f2172bf62a38de6fce46746a5f2cc8ed674de820
phugh <peter@phugh.es>
2017-08-24 12:33:59 +0100

    1.0.0-rc.1 - Add options - Improve documentation - Abstract shared function to external module (lex-helpers) - Switch to Google code style - Swtich from depreciated 'root' to 'global'

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-# bigfive - Node.js based Big Five Personality Assessment
+# bigFive - Node.js based Big Five Personality Assessment
 
-Analyse the Big Five personality traits from strings.
+Analyse the Five Factor Model ("Big Five") personality traits from strings.
 
 ## Usage
 ```Javascript
-const b5 = require('bigfive');
+const bf = require('bigfive');
+const opts = {   // These are the default options
+ 'encoding': 'binary',
+ 'max': Number.POSITIVE_INFINITY,
+ 'min': Number.NEGATIVE_INFINITY,
+ 'nGrams': true,
+ 'output': 'ocean',
+ 'places': 16,
+ 'sortBy': 'lex',
+ 'wcGrams': false,
+};
 const str = "A big long string of text...";
-const personality = b5(str);
+const personality = bf(str, opts);
 console.log(personality)
 ```
 
-## Output Example
+## Default Output Example
 ```Javascript
 {
   O: 0.5491229166666666,
@@ -22,14 +32,106 @@ console.log(personality)
 ```
 Errors return null
 
+## The Options Object
+
+The options object is optional and provides a number of controls to allow you to tailor the output to your needs. However, for general use it is recommended that all options are left to their defaults.
+
+### 'encoding'
+
+**String - valid options: 'binary' (default), 'freq'**
+
+Controls how the lexical value is calculated.
+
+Binary is simply the addition of lexical weights, i.e. word1 + word2 + word3.
+
+Frequency encoding takes the overall wordcount and word frequency into account, i.e. (word frequency / word count) * weight.
+
+### 'max' and 'min'
+
+**Float**
+
+Exclude words that have weights above the max threshold or below the min threshold.
+
+By default these are set to infinity, ensuring that no words from the lexicon are excluded.
+
+### 'nGrams'
+
+**Boolean - valid options: true (default) or false**
+
+n-Grams are contiguous pieces of text, bi-grams being chunks of 2, tri-grams being chunks of 3, etc.
+
+Use the nGrams option to include (true) or exclude (false) n-grams. For accuracy it is recommended that n-grams are included, however including n-grams for very long strings can detrement performance.
+
+### 'output'
+
+**String - valid options: 'ocean' (default), 'matches', 'full'**
+
+'ocean' (default) returns an object with O,C,E,A,N keys and lexical value properties - see 'default output example' above.
+
+'matches' returns an object with O,C,E,A,N keys, each with an array of lexical match information within. See 'matches output example' below.
+
+'full' returns both of the above in one object with two keys, 'values' and 'matches'. 'values' = the 'ocean' object above. 'matches' = the matches object above.
+
+### 'places'
+
+**Number - valid options between 0 and 20 inclusive.**
+
+Number of decimal places to limit outputted values to.
+
+The default is 16 decimal places as this is accuracy level the lexicon data provides.
+
+### 'sortBy'
+
+**String - valid options: 'lex' (default), 'weight', or 'freq'**
+
+If 'output' = 'matches', this option can be used to control how the outputted array is sorted.
+
+'lex' (default) sorts by final lexical value, (N.B. when using binary encoding [see 'encoding' above] the lexical value and the weight are identical.)
+
+'weight' sorts the array by the matched words initial weight.
+
+'freq' sorts by word frequency, i.e. the most used words appear first.
+
+### 'wcGrams'
+
+**Boolean - valid options: true or false (default)**
+
+When set to true, the output from the nGrams option will be added to the word count.
+
+For accuracy it is recommended that this is set to false.
+
+## {output: 'matches'} output example
+
+```javascript
+{
+  O:
+    [
+      [ 'magnificent', 1, -192.0206116, -1.3914537072463768 ],
+      [ 'capital', 1, -133.9311307, -0.9705154398550726 ],
+      [ 'note', 3, -34.83417005, -0.7572645663043478 ],
+      [ 'america', 2, -49.21227355, -0.7132213557971014 ],
+      [ 'republic', 1, -75.5720402, -0.5476234797101449 ]
+    ],
+  C:
+    [
+      ....
+    ],
+  ...
+};
+```
+The items in each array represent: [0] - the word, [1] - number of appearances in string (frequency), [2] - the word's weight, [3] - its final lexical value.
+
+
 ## Acknowledgements
 
-### Lexicon
-Using the Big Five lexica from http://www.wwbp.org/data.html
+### References
+Based on [Schwartz, H. A., Eichstaedt, J. C., Kern, M. L., Dziurzynski, L., Ramones, S. M., Agrawal, M., Shah, A., Kosinski, M., Stillwell, D., Seligman, M. E., & Ungar, L. H. (2013). Personality, Gender, and Age in the Language of Social Media: The Open-Vocabulary Approach. PLOS ONE, 8(9), e73791.](http://journals.plos.org/plosone/article/file?id=10.1371/journal.pone.0073791&type=printable)
 
-Used under the [Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported](http://creativecommons.org/licenses/by-nc-sa/3.0/)
+### Lexicon
+Using the Big Five lexicon data from [WWBP](http://www.wwbp.org/lexica.html) under the [Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported](http://creativecommons.org/licenses/by-nc-sa/3.0/).
 
 ## Licence
-(C) 2017 [P. Hughes](www.phugh.es)
+(C) 2017 [P. Hughes](https://www.phugh.es).
 
-[Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported](http://creativecommons.org/licenses/by-nc-sa/3.0/)
+[Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported](http://creativecommons.org/licenses/by-nc-sa/3.0/).
+
--- index.js
+++ index.js
@@ -1,178 +1,180 @@
 /**
- * bigfive
- * v0.4.0
+ * bigFive
+ * v1.0.0-rc.1
  *
- * Analyse Big Five personality traits from strings.
+ * Analyse the Five Factor Model ("Big Five") personality traits from strings.
  *
  * Help me make this better:
- * https://github.com/phugh/bigfive
+ * https://github.com/phugh/bigFive
  *
- * Using the Big Five lexica data from http://www.wwbp.org/data.html
- * Used under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported licence
+ * Using the Big Five lexica data from http://www.wwbp.org/ under the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported licence
  *
  * (C) 2017 P. Hughes
  * Licence : Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
  * http://creativecommons.org/licenses/by-nc-sa/3.0/
  *
  * Usage example:
- * const b5 = require('bigfive');
- * const str = "A big long string of text...";
- * const personality = b5(str);
- * console.log(personality)
+ * const bf = require('bigFive');
+ * const options = { // These are the default options
+ *  'encoding': 'binary',
+ *  'max': Number.POSITIVE_INFINITY,
+ *  'min': Number.NEGATIVE_INFINITY,
+ *  'nGrams': true,
+ *  'output': 'ocean',
+ *  'places': 16,
+ *  'sortBy': 'lex',
+ *  'wcGrams': false,
+ * };
+ * const string = 'A big long string of text...';
+ * const personality = bf(string, options);
+ * console.log(personality);
+ *
+ * See README.md for help and valid options.
  *
  * @param {string} str input string
- * @return {Object} object with O,C,E,A,N keys
+ * @param {Object} opts options object
+ * @return {Object} object with O, C, E, A, N keys
  */
 
 'use strict'
-;(function () {
-  const root = this
-  const previous = root.bigfive
+;(function() {
+  const global = this;
+  const previous = global.bigFive;
 
-  let lexicon = root.lexicon
-  let simplengrams = root.simplengrams
-  let tokenizer = root.tokenizer
+  let lexicon = global.lexicon;
+  let simplengrams = global.simplengrams;
+  let tokenizer = global.tokenizer;
+  let lexHelpers = global.lexHelpers;
 
   if (typeof lexicon === 'undefined') {
     if (typeof require !== 'undefined') {
-      lexicon = require('./data/lexicon.json')
-      simplengrams = require('simplengrams')
-      tokenizer = require('happynodetokenizer')
-    } else throw new Error('bigfive requires happynodetokenizer and simplengrams, and ./data/lexicon.json')
+      lexicon = require('./data/lexicon.json');
+      simplengrams = require('simplengrams');
+      tokenizer = require('happynodetokenizer');
+      lexHelpers = require('lex-helpers');
+    } else throw new Error('bigFive required modules not found!');
   }
 
-  /**
-   * Get the indexes of duplicate elements in an array
-   * @function indexesOf
-   * @param  {Array} arr input array
-   * @param  {string} el element to test against
-   * @return {Array} array of indexes
-   */
-  const indexesOf = (arr, el) => {
-    const idxs = []
-    let i = arr.length
-    while (i--) {
-      if (arr[i] === el) {
-        idxs.unshift(i)
-      }
-    }
-    return idxs
-  }
+  const arr2string = lexHelpers.arr2string;
+  const prepareMatches = lexHelpers.prepareMatches;
+  const getMatches = lexHelpers.getMatches;
+  const calcLex = lexHelpers.calcLex;
 
   /**
-   * Combines multidimensional array elements into strings
-   * @function arr2string
-   * @param  {Array} arr input array
-   * @return {Array} output array
-   */
-  const arr2string = arr => {
-    let i = 0
-    const len = arr.length
-    const result = []
-    for (i; i < len; i++) {
-      result.push(arr[i].join(' '))
-    }
-    return result
-  }
-
-  /**
-  * Loop through lexicon and match against array
-  * @function getMatches
-  * @param  {Array} arr token array
-  * @return {Object} object of matches
-  */
-  const getMatches = arr => {
-    // error prevention
-    if (arr == null) return null
-    // loop through categories in lexicon
-    const matches = {}
-    let category
-    for (category in lexicon) {
-      if (!lexicon.hasOwnProperty(category)) continue
-      let match = []
-      let word
-      let data = lexicon[category]
-      // loop through words in category
-      for (word in data) {
-        if (!data.hasOwnProperty(word)) continue
-        let weight = data[word]
-        // if word from input matches word from lexicon ...
-        if (arr.indexOf(word) > -1) {
-          let count = indexesOf(arr, word).length // number of times the word appears in the input text
-          match.push([word, count, weight])
-        }
-      }
-      matches[category] = match
-    }
-    return matches
-  }
-
-  /**
-  * Calculate the total lexical value of matches
-  * @function calcLex
-  * @param {Object} obj matches object
-  * @return {number} lexical value
-  */
-  const calcLex = obj => {
-    if (obj == null) return null
-    let lex = 0
-    let word
-    for (word in obj) {
-      if (!obj.hasOwnProperty(word)) continue
-      // weight + weight + weight etc
-      lex += Number(obj[word][2])
-    }
-    return lex
-  }
-
-  /**
-  * @function bigfive
+  * @function bigFive
   * @param {string} str input string
+  * @param {Object} opts options object
   * @return {Object} object of lexical values
   */
-  const bigfive = str => {
-    // error prevention
-    if (str == null) return null
-    if (typeof str !== 'string') str = str.toString()
-    // trim whitespace and convert to lowercase
-    str = str.toLowerCase().trim()
-    // convert our string to tokens
-    let tokens = tokenizer(str)
-    // return null on no tokens
-    if (tokens == null) return null
-    // get n-grams
-    const ngrams = []
-    ngrams.push(arr2string(simplengrams(str, 2)))
-    ngrams.push(arr2string(simplengrams(str, 3)))
-    const nLen = ngrams.length
-    let i = 0
-    for (i; i < nLen; i++) {
-      tokens = tokens.concat(ngrams[i])
+  const bigFive = (str, opts) => {
+    // no string return null
+    if (!str) {
+      console.error('bigFive: no string found. Aborting.');
+      return null;
     }
+    // if str isn't a string, make it into one
+    if (typeof str !== 'string') str = str.toString();
+    // trim whitespace and convert to lowercase
+    str = str.toLowerCase().trim();
+    // options defaults
+    if (!opts || typeof opts !== 'object') {
+      opts = {
+        'encoding': 'binary',
+        'max': Number.POSITIVE_INFINITY,
+        'min': Number.NEGATIVE_INFINITY,
+        'nGrams': true,
+        'output': 'ocean',
+        'places': 16,
+        'sortBy': 'lex',
+        'wcGrams': false,
+      };
+    }
+    opts.encoding = opts.encoding || 'binary';
+    opts.max = opts.max || Number.POSITIVE_INFINITY;
+    opts.min = opts.min || Number.NEGATIVE_INFINITY;
+    opts.nGrams = opts.nGrams || true;
+    opts.output = opts.output || 'ocean';
+    opts.places = opts.places || 16;
+    opts.sortBy = opts.sortBy || 'lex';
+    opts.wcGrams = opts.wcGrams || false;
+    const encoding = opts.encoding;
+    const output = opts.output;
+    const places = opts.places;
+    const sortBy = opts.sortBy;
+    // convert our string to tokens
+    let tokens = tokenizer(str);
+    // if there are no tokens return null
+    if (!tokens) {
+      console.warn('bigFive: no tokens found. Returned null.');
+      return null;
+    }
+    // get wordcount before we add ngrams
+    let wordcount = tokens.length;
+    // get n-grams
+    if (opts.nGrams) {
+      const bigrams = arr2string(simplengrams(str, 2));
+      const trigrams = arr2string(simplengrams(str, 3));
+      tokens = tokens.concat(bigrams, trigrams);
+    }
+    // recalculate wordcount if wcGrams is true
+    if (opts.wcGrams) wordcount = tokens.length;
     // get matches from array
-    const matches = getMatches(tokens, lexicon)
-    // calculate lexical useage
-    let ocean = {}
-    ocean.O = calcLex(matches.O)
-    ocean.C = calcLex(matches.C)
-    ocean.E = calcLex(matches.E)
-    ocean.A = calcLex(matches.A)
-    ocean.N = calcLex(matches.N)
-    // return wellbeing object
-    return ocean
-  }
+    const matches = getMatches(tokens, lexicon, opts.min, opts.max);
+    const ocean = {};
+    if (output === 'full') {
+      // return one object with both matches and values
+      ocean.matches = {};
+      ocean.values = {};
+      ocean.values.O = calcLex(matches.O, 0, places, encoding, wordcount);
+      ocean.values.C = calcLex(matches.C, 0, places, encoding, wordcount);
+      ocean.values.E = calcLex(matches.E, 0, places, encoding, wordcount);
+      ocean.values.A = calcLex(matches.A, 0, places, encoding, wordcount);
+      ocean.values.N = calcLex(matches.N, 0, places, encoding, wordcount);
+      ocean.matches.O = prepareMatches(matches.O, sortBy, wordcount, places,
+          encoding);
+      ocean.matches.C = prepareMatches(matches.C, sortBy, wordcount, places,
+          encoding);
+      ocean.matches.E = prepareMatches(matches.E, sortBy, wordcount, places,
+          encoding);
+      ocean.matches.A = prepareMatches(matches.A, sortBy, wordcount, places,
+          encoding);
+      ocean.matches.N = prepareMatches(matches.N, sortBy, wordcount, places,
+          encoding);
+    } else if (output === 'matches') {
+      // return match object if requested
+      ocean.O = prepareMatches(matches.O, sortBy, wordcount, places, encoding);
+      ocean.C = prepareMatches(matches.C, sortBy, wordcount, places, encoding);
+      ocean.E = prepareMatches(matches.E, sortBy, wordcount, places, encoding);
+      ocean.A = prepareMatches(matches.A, sortBy, wordcount, places, encoding);
+      ocean.N = prepareMatches(matches.N, sortBy, wordcount, places, encoding);
+    } else {
+      // return lexical useage
+      if (output !== 'ocean') {
+        console.warn('bigFive: output option ("' + output +
+            '") is invalid, defaulting to "ocean".');
+      }
+      ocean.O = calcLex(matches.O, 0, places, encoding, wordcount);
+      ocean.C = calcLex(matches.C, 0, places, encoding, wordcount);
+      ocean.E = calcLex(matches.E, 0, places, encoding, wordcount);
+      ocean.A = calcLex(matches.A, 0, places, encoding, wordcount);
+      ocean.N = calcLex(matches.N, 0, places, encoding, wordcount);
+    }
+    // return ocean object
+    return ocean;
+  };
 
-  bigfive.noConflict = function () {
-    root.bigfive = previous
-    return bigfive
-  }
+  bigFive.noConflict = function() {
+    global.bigFive = previous;
+    return bigFive;
+  };
 
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
-      exports = module.exports = bigfive
+      exports = module.exports = bigFive;
     }
-    exports.bigfive = bigfive
+    exports.bigFive = bigFive;
   } else {
-    root.bigfive = bigfive
+    global.bigFive = bigFive;
   }
-}).call(this)
+}).call(this);
